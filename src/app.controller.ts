5c48d455934e97ae9fd658be0bc74b39f4d65b35
 Marco Figueroa <marcofigueroa23@icloud.com>
2026-04-20 23:19:12 -0400

    refactor(health): [APIPET-2] changing the app health check for db property

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Logger, Res } from '@nestjs/common';
+import { AppService } from './app.service.js';
+import type { Response } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  private readonly logger = new Logger(AppController.name);
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello(@Res() res: Response): Promise<void> {
+    try {
+      this.logger.log('Requesting health check of the system');
+      const service = await this.appService.getProperty('SERVICE NAME');
+      const status = await this.appService.getProperty('SERVICE STATUS');
+      this.logger.log('Health check successful');
+      res.status(200).json({ service, status });
+    } catch (err) {
+      const error = new Error(err as string);
+      this.logger.error(
+        `An error occurred during health check: ${error.message}`,
+      );
+      res.status(500).json({
+        service: 'API PetSecure',
+        status: 'INNACTIVE',
+      });
+    }
   }
 }
--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaClient } from './generated/prisma/client';
+import { PrismaClient } from './generated/prisma/client.js';
 import { PrismaPg } from '@prisma/adapter-pg';
+import 'dotenv/config.js';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
